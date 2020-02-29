2c22015957b7914bbe29864cf20e29a2f1fc9a43
Shaun Gedye <sgedye88@yahoo.com.au>
2020-02-29 12:56:20 +1100

    Updates

---
+++ precache-manifest.8bee30bdf23c6f12a255ff8472713cbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe81fd7682b248a231b7d5ed56b2ce81",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "821e3680b7de5ab89cf7",
+    "url": "/drum-machine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef792312f98fef4740b7",
+    "url": "/drum-machine/static/js/2.467ed493.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drum-machine/static/js/2.467ed493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821e3680b7de5ab89cf7",
+    "url": "/drum-machine/static/js/main.e9cb50f3.chunk.js"
+  },
+  {
+    "revision": "0304f7655c757c5eccfe",
+    "url": "/drum-machine/static/js/runtime-main.0bc21d53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
